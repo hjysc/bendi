e2a69c18dc0fbbae63a636d5443aaca9646aff94
hjysc <91966383+hjysc@users.noreply.github.com>
2024-05-10 00:26:10 +0800

    Update local.sh

--- local.sh
+++ local.sh
@@ -605,7 +605,7 @@ if [[ "$(nproc)" -le "12" ]];then
     ECHOG "WSL临时路径编译中"
     PATH=/usr/local/sbin:/usr/local/bin:/usr/sbin:/usr/bin:/sbin:/bin make -j$(nproc) |& tee ${HOME_PATH}/build_logo/build.log 2>&1
   else
-     make V=s -j$(nproc) |& tee ${HOME_PATH}/build_logo/build.log 2>&1
+     make -j$(nproc) |& tee ${HOME_PATH}/build_logo/build.log 2>&1
   fi
 else
   ECHOGG "您的CPU线程超过或等于16线程，强制使用16线程进行编译固件,请耐心等候..."
